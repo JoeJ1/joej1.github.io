c6cf3217997c544b4f08acb4a14a7a94d88d0df0
JoeJ1 <joeedwardharry1@gmail.com>
2017-11-23 15:26:37 +0000

    Optimisation

--- morseCodeTranslator/morseCodeTranslator.js
+++ morseCodeTranslator/morseCodeTranslator.js
@@ -4,94 +4,17 @@
         var textArray = [];
         textArray = text.split('');
         var morseArray = [];
+        var morseTranslateArray = ['/','._','_...','_._.','_..','.','.._.','__.','....','..','.___','_._','._..','__','_.','___','.__.',
+                          '__._','._.','...','_','.._','..._','.__','_.._','_.__','__..','_____','.____','..___''...__',
+                          '...._','.....','_....','__...','___..','____.','._._._','__..__','..__..','.____.','_._.__'];
+        var englishTranslateArray = [' ','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v',
+                            'w','x','y','z','0','1','2','3','4','5','6','6','7','8','9','.',',','?','\'','!'];
         for(i = 0;i<textArray.length; i++){
-          if(textArray[i] == ' '){
-            morseArray.push('/');
-          }else if(textArray[i].toLowerCase() == "a"){
-            morseArray.push('._');
-          }else if(textArray[i].toLowerCase() == "b"){
-            morseArray.push('_...');
-          }else if(textArray[i].toLowerCase() == "c"){
-            morseArray.push('_._.');
-          }else if(textArray[i].toLowerCase() == "d"){
-            morseArray.push('_..');
-          }else if(textArray[i].toLowerCase() == "e"){
-            morseArray.push('.');
-          }else if(textArray[i].toLowerCase() == "f"){
-            morseArray.push('.._.');
-          }else if(textArray[i].toLowerCase() == "g"){
-            morseArray.push('__.');
-          }else if(textArray[i].toLowerCase() == "h"){
-            morseArray.push('....');
-          }else if(textArray[i].toLowerCase() == "i"){
-            morseArray.push('..');
-          }else if(textArray[i].toLowerCase() == "j"){
-            morseArray.push('.___');
-          }else if(textArray[i].toLowerCase() == "k"){
-            morseArray.push('_._');
-          }else if(textArray[i].toLowerCase() == "l"){
-            morseArray.push('._..');
-          }else if(textArray[i].toLowerCase() == "m"){
-            morseArray.push('__');
-          }else if(textArray[i].toLowerCase() == "n"){
-            morseArray.push('_.');
-          }else if(textArray[i].toLowerCase() == "o"){
-            morseArray.push('___');
-          }else if(textArray[i].toLowerCase() == "p"){
-            morseArray.push('.__.');
-          }else if(textArray[i].toLowerCase() == "q"){
-            morseArray.push('__._');
-          }else if(textArray[i].toLowerCase() == "r"){
-            morseArray.push('._.');
-          }else if(textArray[i].toLowerCase() == "s"){
-            morseArray.push('...');
-          }else if(textArray[i].toLowerCase() == "t"){
-            morseArray.push('_');
-          }else if(textArray[i].toLowerCase() == "u"){
-            morseArray.push('.._');
-          }else if(textArray[i].toLowerCase() == "v"){
-            morseArray.push('..._');
-          }else if(textArray[i].toLowerCase() == "w"){
-            morseArray.push('.__');
-          }else if(textArray[i].toLowerCase() == "x"){
-            morseArray.push('_.._');
-          }else if(textArray[i].toLowerCase() == "y"){
-            morseArray.push('_.__');
-          }else if(textArray[i].toLowerCase() == "z"){
-            morseArray.push('__..');
-          }else if(textArray[i].toLowerCase() == "0"){
-            morseArray.push('_____');
-          }else if(textArray[i].toLowerCase() == "1"){
-            morseArray.push('.____');
-          }else if(textArray[i].toLowerCase() == "2"){
-            morseArray.push('..___');
-          }else if(textArray[i].toLowerCase() == "3"){
-            morseArray.push('...__');
-          }else if(textArray[i].toLowerCase() == "4"){
-            morseArray.push('...._');
-          }else if(textArray[i].toLowerCase() == "5"){
-            morseArray.push('.....');
-          }else if(textArray[i].toLowerCase() == "6"){
-            morseArray.push('_....');
-          }else if(textArray[i].toLowerCase() == "7"){
-            morseArray.push('__...');
-          }else if(textArray[i].toLowerCase() == "8"){
-            morseArray.push('___..');
-          }else if(textArray[i].toLowerCase() == "9"){
-            morseArray.push('____.');
-          }else if(textArray[i].toLowerCase() == "."){
-            morseArray.push('._._._')
-          }else if(textArray[i].toLowerCase() == ","){
-            morseArray.push('__..__')
-          }else if(textArray[i].toLowerCase() == "?"){
-            morseArray.push('..__..')  
-          }else if(textArray[i].toLowerCase() == "\'"){
-            morseArray.push('.____.')
-          }else if(textArray[i].toLowerCase() == "!"){
-            morseArray.push('_._.__')
-          }else{
-            morseArray.push('!');
-          }
+            try{
+                  morseArray.push(morseTranslateArray(englishTranslateArray.indexOf(textArray.toString())))
+            }catch(err){
+                  morseArray.push('!')
+            }
         }
         document.getElementById('textOutput').innerHTML = morseArray.join(' ');
       }
