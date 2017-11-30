2b888b3900bfa56f8a1a9f940509a834e83f28f8
JoeJ1 <joeedwardharry1@gmail.com>
2017-11-30 15:23:26 +0000

    Added binary to english functionality
    (hopefully) I didn't get a chance to test this.

--- binaryTranslator/binaryTranslator.js
+++ binaryTranslator/binaryTranslator.js
@@ -1,4 +1,4 @@
-var englishArray = [" ","!","\"","#","$","%","&","\'","(",")","*","+", ",","-",".","/","0","1","2","3","4","5","6","7","8","9",":",";","<","=",">","?","@","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","[","\\","]","^","_","`","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","{","|","}","~"]
+var englishArray = [" ","!","\"","#","$","%","&","\'","(",")","*","+", ",","-",".","/","0","1","2","3","4","5","6","7","8","9",":",";","\<","=","\>","?","@","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","[","\\","]","^","_","`","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","{","|","}","~"]
 
 //Takes text as input and returns array of corresponding ascii values
 function textToAscii(input){
@@ -26,9 +26,44 @@ function asciiToBinary(inputArray){
     }
     binaryArray[i] = binaryArray[i].join('');
   }
-  return(binaryArray.join(''))
+  return(binaryArray.join(''));
+}
+
+function binaryToAscii(input){ //input must be string of 8 bit binary
+  inputArray = input.match(/.{1,8}/g);
+  outputArray = [];
+  for(i = 0;i<inputArray.length;i++){
+    inputArray[i] = inputArray[i].split('');
+    currentVal = 0;
+    for(j = 7;j>0;j--){
+      tempVal += parseInt(inputArray[i][j]);
+    }
+    outputArray[i].push(currentVal);
+  }
+  return(outputArray)
+}
+
+function asciiToText(inputArray){
+  outputArray = []
+  for(i = 0;i<inputArray.length;i++){
+    outputArray.push(englishArray[inputArray[i]-33])
+  }
+  return(outputArray.join(''))
+}
+
+function toggleMode(){
+  var toggleButton = document.getElementById('toggleButton');
+  if(toggleButton.innerHTML == 'English to Binary'){
+    toggleButton.innerHTML = 'Binary to English';
+  }else{
+    toggleButton.innerHTML = 'English to Binary';
+  }
 }
 
 function run(){
-  document.getElementById('textOutput').innerHTML = asciiToBinary(textToAscii(document.getElementById('textInput').value));
+  if(document.getElementById('toggleButton').innerHTMl = "English to Binary"){
+    document.getElementById('textOutput').innerHTML = asciiToBinary(textToAscii(document.getElementById('textInput').value));
+  }else{
+    document.getElementById('textOutput').innerHTMl = asciiToText(binaryToAscii(document.getElementById('textInput').value));
+  }
 }
