4659045030b7bc51a3c978ca26996dcd55703a68
JoeJ1 <joeedwardharry1@gmail.com>
2017-12-13 21:01:26 +0000

    Changed to sidebar on index.html

---
+++ main.js
@@ -0,0 +1,5 @@
+function toggleSidebar(){
+  var sidebarDisplayType = document.getElementsByClassName('sidebar')[0].style.display
+  console.log(document.getElementsByClassName('sidebar')[0].style.display)
+  console.log('display: '+sidebarDisplayType)
+}
