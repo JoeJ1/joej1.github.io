996d47ac954e3a74060ec577b53487d1678aa9e8
JoeJ1 <joeedwardharry1@gmail.com>
2017-12-18 11:09:36 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 function hideSidebarButtons(){
   sidebarButtons = document.getElementsByClassName('sidebarButton');
   for(i=0;i<sidebarButtons.length;i++){
-    sidebarButtons[i].style.width = "0px";
     sidebarButtons[i].style.padding = "0px";
+	  sidebarButtons[i].style.left = "-250px"
   }
 }
 function showSidebarButtons(){
   sidebarButtons = document.getElementsByClassName('sidebarButton');
   for(i=0;i<sidebarButtons.length;i++){
-    sidebarButtons[i].style.width = "245px";
+	  sidebarButtons[i].style.left = "0px"
   }
 }
+
