9c6ac1de5cc319fba8b733fdda07c8c630231c42
JoeJ1 <joeedwardharry1@gmail.com>
2017-12-23 11:50:27 +0000

    Enabled scrolling for the sidebar

--- main.js
+++ main.js
@@ -4,13 +4,12 @@ function hideSidebarButtons(){
     sidebarButtons[i].style.padding = "0px";
 	  sidebarButtons[i].style.left = "-250px";
   }
-  document.getElementsByClassName('closeIcon')[0].style.left="-24px";
+  document.getElementsByClassName('closeIcon')[0].style.left="-30px";
 }
 function showSidebarButtons(){
   sidebarButtons = document.getElementsByClassName('sidebarButton');
   for(i=0;i<sidebarButtons.length;i++){
 	  sidebarButtons[i].style.left = "0px"
   }
-  document.getElementsByClassName('closeIcon')[0].style.left="226px";
+  document.getElementsByClassName('closeIcon')[0].style.left="212px";
 }
-
